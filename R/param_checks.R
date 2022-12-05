8e0cc8f9a2326b547d172f07b23b4a3a90724bd5
Eric Nantz <thercast@gmail.com>
2022-12-05 12:06:23 -0500

    fix brackets

--- R/param_checks.R
+++ R/param_checks.R
@@ -1,6 +1,6 @@
 check_max <- function(x, arg_name = "max") {
   if (!rlang::is_scalar_double(x) || is.na(x) || x < 1 || x > 1000) {
-    cli::cli_abort("{.arg {arg_name} must be a single value between 1 and 1000") # noling
+    cli::cli_abort("{.arg {arg_name}} must be a single value between 1 and 1000") # noling
   }
   x
 }
@@ -8,7 +8,7 @@ check_max <- function(x, arg_name = "max") {
 check_boolean <- function(x, arg_name = c("clean", "fulltext", "desc", "similar", "pretty")) {
   arg_name <- rlang::arg_match(arg_name)
   if (!rlang::is_bool(x)) {
-    cli::cli_abort("{.arg {arg_name} must be either TRUE or FALSE")
+    cli::cli_abort("{.arg {arg_name}} must be either TRUE or FALSE")
   }
   if (!x) {
     return(NULL)
@@ -18,8 +18,8 @@ check_boolean <- function(x, arg_name = c("clean", "fulltext", "desc", "similar"
 }
 
 check_datetime <- function(x, arg_name = "since") {
-  if (!is.null(x) && !grepl("^\\d{4}-\\d{2}-\\d{2$", x)) {
-    cli::cli_abort("{.arg {arg_name} must be a string with format YYYY-MM-DD")
+  if (!is.null(x) && !grepl("^\\d{4}-\\d{2}-\\d{2}$", x)) {
+    cli::cli_abort("{.arg {arg_name}} must be a string with format YYYY-MM-DD")
   }
   create_epoch_time(x)
 }
