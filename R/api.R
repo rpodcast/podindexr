aa0b50bd3683e36ab7652c1ffd9fdc907b4bd57f
Eric Nantz <thercast@gmail.com>
2022-12-05 11:30:10 -0500

    streamline request foundation function

--- R/api.R
+++ R/api.R
@@ -1,30 +1,44 @@
 #' Create a Request for the podcastindex API
 #'
-#' @param type string for API request operation
-#' @param resource string for endpoint associated with the specified `type`
-#' @param ... optional named query parameters for the `resource`
-#'
 #' @return An [httr2::request] object
 #' @noRd
-#'
-req_podcastindex <- function(
-  type = c("podcasts", "episodes", "recent", "value", "stats", "categories", "search"),
-  resource,
-  ...) {
-  params <- list(...)
+req_podcastindex <- function() {
   epoch_time <- create_epoch_time()
-
-  resp <- httr2::request("https://api.podcastindex.org/api/1.0") |>
-    httr2::req_url_path_append(type, resource) |>
-    httr2::req_url_query(!!!params) |>
+  httr2::request("https://api.podcastindex.org/api/1.0") |>
     httr2::req_user_agent(create_user_agent()) |>
     httr2::req_headers(
       `X-Auth-Date` = epoch_time,
       `X-Auth-Key` = get_podcastindex_api_keys()$api_key,
       Authorization = create_hash(epoch_time),
       Accept = "application/json"
-    ) |>
-    httr2::req_perform()
+    )
+}
 
-  return(resp)
+#' Create data frame from podcastindex API request
+#'
+#' @param req An [httr2::request] object created by `req_podcastindex`
+#' @param slot string indicating which slot of the JSON response
+#'   contains the result of interest. Value depends on the endpoint
+#'   used in the request.
+#'
+#' @return tibble with request results
+#' @noRd
+process_podcastindex_req <- function(req, slot) {
+  req_json <- httr2::resp_body_json(req)
+
+  res_list <- req_json |>
+    purrr::pluck(slot)
+
+  res_list <- purrr::compact(res_list)
+
+  if (is.null(names(res_list))) {
+    res_df <- purrr::map_dfr(res_list, ~{
+      x_list <- purrr::compact(.x)
+      tibble::as_tibble(purrr::modify_if(x_list, ~length(.x) > 1, list))
+    })
+  } else {
+    res_df <- tibble::as_tibble(purrr::modify_if(res_list, ~length(.x) > 1, list))
+  }
+
+  return(res_df)
 }
